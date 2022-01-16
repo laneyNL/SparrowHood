44bb8c67ff19e5119048f53c668de69e4f01b688
Laney <laneyluong@gmail.com>
2022-01-15 17:41:42 -0800

    update apikey import

--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -1,6 +1,7 @@
+
 export const fetchAsset = (symbol) => {
   return $.ajax({
     method: 'GET', 
-    urL: `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}`
+    urL: `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${process.env.REACT_APP_VARIABLE}`
   })
 }
