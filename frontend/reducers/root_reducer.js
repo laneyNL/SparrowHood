cc0ed9cf4be62360647959b2947925f78c379a29
Laney <laneyluong@gmail.com>
2022-01-21 14:26:24 -0800

    add entities reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import sessionReducer from "./session_reducer";
+import entitiesReducer from "./entities_reducer";
 
 const RootReducer = combineReducers({
   session: sessionReducer,
-
+  entities: entitiesReducer
 })
 
 export default RootReducer;
