d8473e068debde02f62247b9d0e568153af6e428
Laney <laneyluong@gmail.com>
2022-01-21 11:58:57 -0800

    create thunk action creators

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,4 +1,4 @@
-import { signup, login, logout } from '../util/session_api_util';
+import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
@@ -8,13 +8,28 @@ const receiveUser = (user) => ({
   type: RECEIVE_CURRENT_USER,
   user
 })
+
 const logoutUser = (message) => ({
   type: LOGOUT_CURRENT_USER,
   message
 })
+
 const receiveErrors = (errors) => ({
   type: RECEIVE_ERRORS,
   errors
 })
 
-export
+export const signup = (user) => (dispatch) => {
+  return SessionApiUtil.signup(user)
+    .then((user) => dispatch(receiveUser(user)), (errors) => dispatch(receiveErrors(errors.responseJSON)))
+}
+
+export const login = (user) => (dispatch) => {
+  return SessionApiUtil.login(user)
+    .then((user) => dispatch(receiveUser(user)), (errors) => dispatch(receiveErrors(errors.responseJSON)))
+}
+
+export const logout = () => (dispatch) => {
+  return SessionApiUtil.logout()
+    .then((message) => dispatch(logoutUser(message)), (errors) => dispatch(receiveErrors(errors.responseJSON)))
+}
