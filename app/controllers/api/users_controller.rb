8f8c047bdb3cc5e95d5f48013b7faa1a9192cd41
Laney <laneyluong@gmail.com>
2022-01-21 11:37:25 -0800

    update user auth

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -16,7 +16,7 @@ class Api::UsersController < ApplicationController
     if @user
       render :show
     else
-      render json: @user.errors.full_messages, status: 404
+      render json: ['User must be logged in'], status: 401
     end
   end
 
