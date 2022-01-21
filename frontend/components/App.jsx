38ad7cae84fc9c76e2dead537056a8c876f44284
Laney <laneyluong@gmail.com>
2022-01-21 15:26:01 -0800

    update react render

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
-
+    <header>
+      <Link to='/' >SparrowHood</Link>
+    </header>
+    <div>
+      Inside App
+    </div>
   </div>
 )
 
