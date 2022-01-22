63983a5be5880c3af872ee80c9a083368b07177b
Laney <laneyluong@gmail.com>
2022-01-22 12:38:00 -0800

    update css

--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 export default class LoginForm extends React.Component {
   constructor(props) {
     super(props);
@@ -25,7 +24,7 @@ export default class LoginForm extends React.Component {
     return (
       <div className='login'>
         <div className='login-img'>
-          <img src="" alt="" /> Placeholder
+          <img src='https://cdn.robinhood.com/assets/generated_assets/web-bundle-lazy-route-prod-experiment/member/632fcb3e7ed928b2a960f3e003d10b44.jpg' alt="" className='login-img' />
         </div>
         <div className='form'>
           
