83659d5265a0bf3cc14554967af47148617ba8c6
Laney <laneyluong@gmail.com>
2022-01-23 23:06:00 -0800

    update icons

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import splashBanner from '../../app/assets/images/splash-phone.png';
 import splashBalloon from '../../app/assets/images/splash-balloon.svg';
 import splashVideo from '../../app/assets/videos/splash-video.mp4';
+import feather from '../../app/assets/images/green-feather.png'
 
 export default class Splash extends React.Component {
   constructor(props) {
@@ -32,9 +33,9 @@ export default class Splash extends React.Component {
     return (
       <div className='splash'>
         <nav>
-          <Link to='/' className='nav-home'>Robinhood</Link>
-          <div className='hide-nav'><a href="https://github.com/laneyNL">GitHub</a></div>
-          <div className='hide-nav'><a href="https://www.linkedin.com/in/laneyluong/">LinkedIn</a></div>
+          <Link to='/' className='nav-home'>Robinhood <img src={feather} alt="feather" id='feather' /></Link>
+          <div className='hide-nav'><a href="https://github.com/laneyNL">GitHub <i className="fab fa-github"></i></a></div>
+          <div className='hide-nav'><a href="https://www.linkedin.com/in/laneyluong/">LinkedIn <i className="fab fa-linkedin"></i></a></div>
           <div className='hide-nav'>Support</div>
           <div className='hide-nav'>Who we are</div>
           <div className='nav-links bold hide-nav'>
@@ -68,8 +69,8 @@ export default class Splash extends React.Component {
         <div className='banner'>
           <div className='left-banner'>
             <h1>Investing for Everyone</h1><br />
-            <p className='banner-info'>Commission-free investing, plus the tools you need to put your money in motion. Sign up and get your first stock for free. Certain limitations and fees apply</p><br />
-            <Link to='/signup' className='signup-button'>Sign Up</Link><br />
+            <p className='banner-info'>Commission-free investing, plus the tools you need to put your money in motion. Sign up and get your first stock for free. Certain limitations and fees apply</p><br /><br />
+            <Link to='/signup' className='signup-button'>Sign Up</Link><br /><br />
             <p className='bold'><i className="fas fa-info-circle"></i> Commissions & Free Stock Disclosures</p>
           </div>
           <div className='right-banner'>
@@ -78,7 +79,9 @@ export default class Splash extends React.Component {
               <video src={splashVideo} autoPlay loop muted preload='auto' id='splashVideo'></video></div>
           </div>
         </div>
-        <div>See our fee schedule to learn more about cost.</div>
+
+        <div className='fee'>See our <span className='underline'>fee schedule</span> to learn more about cost.</div>
+
         <div className='balloon'>
           <div><img src={splashBalloon} alt="hot air balloon" />
           </div>
