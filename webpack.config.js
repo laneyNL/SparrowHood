36f6ffe5a9d19e76876f55df874d605b78ef1934
Laney <laneyluong@gmail.com>
2022-01-26 10:56:37 -0800

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpg|svg|gif|mp3|jpeg|mp4)$/,
+        test: /\.(png|jpg|svg|gif|mp3|mp4)$/,
         type: 'asset/resource'
       }
     ]
