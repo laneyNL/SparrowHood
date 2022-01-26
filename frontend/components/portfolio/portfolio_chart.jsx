32430baa986982dc0ce69363307d9faf964e76d2
Laney <laneyluong@gmail.com>
2022-01-25 22:53:43 -0800

    update gitignore

--- frontend/components/portfolio/portfolio_chart.jsx
+++ frontend/components/portfolio/portfolio_chart.jsx
@@ -18,86 +18,86 @@ export default class PortfolioChart extends React.Component {
       totalValue: '',
       difference: ''
     }
-    // this.updateSummary.bind(this)
+    this.updateSummary.bind(this)
   }
 
-  // componentDidMount() {
-  //   ChartJS.register(
-  //     CategoryScale, LinearScale, PointElement, LineElement, Title,
-  //     Tooltip, Legend);
-  //   this.props.fetchTransactions(this.props.user.id)
-  //     .then(() => this.setState({ 
-  //       totalValue: `$${this.props.transactions[this.props.transactions.length - 1].currentTotal.toFixed(2)}`
-  //     }));
-  // }
+  componentDidMount() {
+    ChartJS.register(
+      CategoryScale, LinearScale, PointElement, LineElement, Title,
+      Tooltip, Legend);
+    this.props.fetchTransactions(this.props.user.id)
+      .then(() => this.setState({ 
+        totalValue: `$${this.props.transactions[this.props.transactions.length - 1].currentTotal.toFixed(2)}`
+      }));
+  }
 
-  // chartData () {
-  //   return {
-  //     labels: this.props.transactions.map(action => action.createdAt),
-  //     datasets: [
-  //       {
-  //         data: this.props.transactions.map(action => action.currentTotal),
-  //         fill: false,
-  //         borderColor: 'green'
-  //       }
-  //     ]
-  //   }
-  // }
+  chartData () {
+    return {
+      labels: this.props.transactions.map(action => action.createdAt),
+      datasets: [
+        {
+          data: this.props.transactions.map(action => action.currentTotal),
+          fill: false,
+          borderColor: 'green'
+        }
+      ]
+    }
+  }
 
-  // chartOptions() {
-  //   const change = (tooltipItem) => {
-  //     let time = new Date(tooltipItem.label)
-  //     return time.toDateString().split(' ').slice(1).join(' ');
-  //   }
+  chartOptions() {
+    const change = (tooltipItem) => {
+      let time = new Date(tooltipItem.label)
+      return time.toDateString().split(' ').slice(1).join(' ');
+    }
  
-  //   return {
-  //     scales: {
-  //       x: {
-  //         ticks: { display: false }
-  //       },
-  //       y: {
-  //         ticks: { display: false }
-  //       }
-  //     },
-  //     plugins: {
-  //       legend: { 
-  //         display: false,
-  //         onHover: (e, legendItem, legend) => {
-  //           this.setState({ difference: 10 })
-  //           console.log('hover', e, legendItem, legend) }
-  //        },
-  //       tooltip: {
-  //         displayColors: false,
-  //         callbacks: {
-  //           label: change,
-  //           labelTextColor: () => 'green',
-  //           labelColor: () => ({
-  //             backgroundColor: 'transparent'
-  //           }),
-  //           title: () => ''
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+    return {
+      scales: {
+        x: {
+          ticks: { display: false }
+        },
+        y: {
+          ticks: { display: false }
+        }
+      },
+      plugins: {
+        legend: { 
+          display: false,
+          onHover: (e, legendItem, legend) => {
+            this.setState({ difference: 10 })
+            console.log('hover', e, legendItem, legend) }
+         },
+        tooltip: {
+          displayColors: false,
+          callbacks: {
+            label: change,
+            labelTextColor: () => 'green',
+            labelColor: () => ({
+              backgroundColor: 'transparent'
+            }),
+            title: () => ''
+          }
+        }
+      }
+    }
+  }
 
-  // updateSummary(event, legendItem, legend) {
-  //   console.log('hover', event, legendItem, legend)
-  //   console.log(...arguments)
-  //   // const change = (tooltipItems) => {
-  //   //   console.log('toolitems', tooltipItems)
-  //   //   let initialTotal = this.props.transactions[0].currentTotal;
-  //   //   let currTotal = tooltipItems[0].currentTotal;
-  //   //   let diff = currTotal - initialTotal;
-  //   //   let percent_diff = (diff / initialTotal) * 100;
-  //   //   const symbol = diff > 0 ? '+' : '-';
-  //   //   return `${symbol}${diff}(${symbol}${percent_diff}%)`;
-  //   // }
-  // }
+  updateSummary(event, legendItem, legend) {
+    console.log('hover', event, legendItem, legend)
+    console.log(...arguments)
+    // const change = (tooltipItems) => {
+    //   console.log('toolitems', tooltipItems)
+    //   let initialTotal = this.props.transactions[0].currentTotal;
+    //   let currTotal = tooltipItems[0].currentTotal;
+    //   let diff = currTotal - initialTotal;
+    //   let percent_diff = (diff / initialTotal) * 100;
+    //   const symbol = diff > 0 ? '+' : '-';
+    //   return `${symbol}${diff}(${symbol}${percent_diff}%)`;
+    // }
+  }
 
-  // onClick() {
+  onClick() {
 
-  // }
+  }
 
   render() {
     return (
@@ -106,12 +106,12 @@ export default class PortfolioChart extends React.Component {
         <div>{this.state.difference}</div>
         <div></div>
         <div className = 'graph'>
-          {/* <Line 
+          <Line 
             data= {this.chartData()}
             height={200} 
             width={600}
             options={this.chartOptions()}
-          /> */}
+          />
         </div>
         <div className='chartOptions'>
           <span className='nav-link'>1D</span>
