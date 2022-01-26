f7bf32c039cbd02576ee364afb85a6e822c5d1cd
Laney <laneyluong@gmail.com>
2022-01-26 15:38:04 -0800

    update portfolio

--- frontend/components/portfolio/portfolio.jsx
+++ frontend/components/portfolio/portfolio.jsx
@@ -25,6 +25,31 @@ export default class Portfolio extends React.Component {
   render() {
     return (
       <div className='portfolio-splash'>
+        <nav className='port-nav'>
+          <Link to='/'><img src={'https://sparrowhood-dev.s3.us-west-1.amazonaws.com/images/green-feather.png'} alt="green feather" id='feather' /></Link>
+          <div><input type="text" placeholder='Search' /></div>
+          <Link to='/' className='white'>Portfolio</Link>
+          <button onClick={this.props.logout}>Logout</button>
+          <Link to='/'>Cash</Link>
+        </nav>
+
+        <div className='portfolio'>
+          <div className='main-chart'>
+            {/* <PortfolioChart fetchTransactions={this.props.fetchTransactions} user={this.props.user} transactions={this.state.transactions}/> */}
+            <div className='buying-power'>
+              <div>Buying Power</div>
+              <div>${this.props.user.buyingPower}</div>
+            </div>
+          </div>
+          <aside className='asset-list'>
+            <p>Stocks</p>
+            {/* {
+              this.state.symbols.map((symbol, idx) => 
+                <AssetListItem symbol={symbol} assets={this.props.assets} key={idx}/>
+                )
+            } */}
+          </aside>
+        </div>
       </div>
     )
   }
