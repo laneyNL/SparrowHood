8cb2f4ea11dd3e70d7b703edc4e8e10e04e80343
Laney <laneyluong@gmail.com>
2022-01-26 16:12:54 -0800

    update chart

--- frontend/components/portfolio/portfolio_chart.jsx
+++ frontend/components/portfolio/portfolio_chart.jsx
@@ -25,92 +25,92 @@ export default class PortfolioChart extends React.Component {
     // this.updateSummary = this.updateSummary.bind(this);
   }
 
-  // chartData () {
-  //   return {
-  //     labels: this.props.transactions.map(action => action.createdAt),
-  //     datasets: [
-  //       {
-  //         data: this.props.transactions.map(action => action.currentTotal),
-  //         fill: false,
-  //         borderColor: 'green'
-  //       }
-  //     ]
-  //   }
-  // }
+  chartData () {
+    return {
+      labels: this.props.transactions.map(action => action.createdAt),
+      datasets: [
+        {
+          data: this.props.transactions.map(action => action.currentTotal),
+          fill: false,
+          borderColor: 'green'
+        }
+      ]
+    }
+  }
 
-  // chartOptions() {
-  //   const change = (tooltipItem) => {
-  //     let time = new Date(tooltipItem.label)
-  //     return time.toDateString().split(' ').slice(1).join(' ');
-  //   }
+  chartOptions() {
+    const change = (tooltipItem) => {
+      let time = new Date(tooltipItem.label)
+      return time.toDateString().split(' ').slice(1).join(' ');
+    }
  
-  //   return {
-  //     scales: {
-  //       x: {
-  //         ticks: { display: false }
-  //       },
-  //       y: {
-  //         ticks: { display: false }
-  //       }
-  //     },
-  //     plugins: {
-  //       legend: { 
-  //         display: false,
-  //         // onHover: (e, legendItem, legend) => {
-  //         //   this.setState({ difference: 10 })
-  //         //   console.log('hover', e, legendItem, legend) }
-  //        },
-  //       tooltip: {
-  //         displayColors: false,
-  //         callbacks: {
-  //           label: change,
-  //           labelTextColor: () => 'green',
-  //           labelColor: () => ({
-  //             backgroundColor: 'transparent'
-  //           }),
-  //           title: () => ''
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+    return {
+      scales: {
+        x: {
+          ticks: { display: false }
+        },
+        y: {
+          ticks: { display: false }
+        }
+      },
+      plugins: {
+        legend: { 
+          display: false,
+          // onHover: (e, legendItem, legend) => {
+          //   this.setState({ difference: 10 })
+          //   console.log('hover', e, legendItem, legend) }
+         },
+        tooltip: {
+          displayColors: false,
+          callbacks: {
+            label: change,
+            labelTextColor: () => 'green',
+            labelColor: () => ({
+              backgroundColor: 'transparent'
+            }),
+            title: () => ''
+          }
+        }
+      }
+    }
+  }
 
-  // updateSummary(event, legendItem, legend) {
-  //   console.log('hover', event, legendItem, legend)
-  //   console.log(...arguments)
-  //   // const change = (tooltipItems) => {
-  //   //   console.log('toolitems', tooltipItems)
-  //   //   let initialTotal = this.props.transactions[0].currentTotal;
-  //   //   let currTotal = tooltipItems[0].currentTotal;
-  //   //   let diff = currTotal - initialTotal;
-  //   //   let percent_diff = (diff / initialTotal) * 100;
-  //   //   const symbol = diff > 0 ? '+' : '-';
-  //   //   return `${symbol}${diff}(${symbol}${percent_diff}%)`;
-  //   // }
-  // }
+  updateSummary(event, legendItem, legend) {
+    console.log('hover', event, legendItem, legend)
+    console.log(...arguments)
+    // const change = (tooltipItems) => {
+    //   console.log('toolitems', tooltipItems)
+    //   let initialTotal = this.props.transactions[0].currentTotal;
+    //   let currTotal = tooltipItems[0].currentTotal;
+    //   let diff = currTotal - initialTotal;
+    //   let percent_diff = (diff / initialTotal) * 100;
+    //   const symbol = diff > 0 ? '+' : '-';
+    //   return `${symbol}${diff}(${symbol}${percent_diff}%)`;
+    // }
+  }
  
-  // onClick() {
+  onClick() {
 
-  // }
+  }
 
   render() {
-    // if (!this.props.transactions.length) return null;
-    // const totalValue = `$${this.props.transactions[this.props.transactions.length - 1].currentTotal.toFixed(2)}`;
-    // const difference = '';
+    if (!this.props.transactions.length) return null;
+    const totalValue = `$${this.props.transactions[this.props.transactions.length - 1].currentTotal.toFixed(2)}`;
+    const difference = '';
     
     return (
       <div className='chart'>
-        {/* <div className='totalValue'>{totalValue}</div>
+        <div className='totalValue'>{totalValue}</div>
         <div className='difference'>{difference}</div>
         <div></div>
-        <div className = 'graph'> */}
+        <div className = 'graph'>
           {/* <Line 
             data= {this.chartData()}
             height={200} 
             width={600}
             options={this.chartOptions()}
           /> */}
-        {/* </div> */}
+        </div>
         <div className='chartOptions'>
           <span className='nav-link'>1D</span>
           <span className='nav-link'>1W</span>
