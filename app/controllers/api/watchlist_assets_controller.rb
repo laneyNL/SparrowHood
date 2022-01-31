5edc545d8f7933a624600c55b0ab4a8704a97363
Laney <laneyluong@gmail.com>
2022-01-30 22:02:32 -0800

    update watchlist controllers and api util

--- app/controllers/api/watchlist_assets_controller.rb
+++ app/controllers/api/watchlist_assets_controller.rb
@@ -1,10 +1,24 @@
 class Api::WatchlistAssetsController < ApplicationController
 
   def create
-
+    @asset = WatchlistAsset.new(asset_params)
+    if @asset.save
+      render :show
+    else  
+      render json: @watchlist.errors.full_messages, status: 404
+    end
   end
 
   def destroy
+    @asset = WatchlistAsset.find_by(id: params[:id])
+    if @asset.destroy
+      render :show
+    else  
+      render json: @asset.errors.full_messages, status: 404
+  end
 
+  private
+  def assets_params
+    params.require(:watchlist_assets).permit(:watchlist_id, :symbol)
   end
 end
--- app/controllers/api/watchlists_controller.rb
+++ app/controllers/api/watchlists_controller.rb
@@ -1,23 +1,43 @@
 class Api::WatchlistsController < ApplicationController
 
   def index
-
+    @watchlists = Watchlist.where(user_id: params[:user_id]).include(:assets)
+    render :index
   end
 
   def show
-
-
+    @watchlist = Watchlist.find_by(id: params[:id])
+    render :show
   end
 
   def create
-
+    @watchlist = Watchlist.new(watchlist_params)
+    if @watchlist.save
+      render :show
+    else  
+      render json: @watchlist.errors.full_messages, status: 404
+    end
   end
 
   def update
-  
+    @watchlist = Watchlist.find_by(id: params[:id])
+    if @watchlist && @watchlist.user_id = current_user.id && @watchlist.update(watchlist_params)
+      render :show
+    else  
+      render json: @watchlist.errors.full_messages, status: 404
+    end
   end
 
   def destroy
+    @watchlist = Watchlist.find_by(id: params[:id])
+    if @watchlist.user_id = current_user.id && @watchlist.destroy
+      render :show
+    else  
+      render json: @watchlist.errors.full_messages, status: 404
+  end
 
+  private
+  def watchlist_params
+    params.require(:watchlist).permit(:user_id, :name)
   end
 end
