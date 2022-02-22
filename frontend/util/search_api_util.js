65852870a7592da471fbbd8f3f5306e5db8a1e91
Laney <laneyluong@gmail.com>
2022-02-22 11:49:32 -0800

    update search for lowercase words

--- frontend/util/search_api_util.js
+++ frontend/util/search_api_util.js
@@ -1,6 +1,6 @@
 export const fetchSearch = (keyword) => {
   return $.ajax({
     method: 'GET',
-    url: `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=${window.alphaAPIKey}`
+    url: `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword.toUpperCase()}&apikey=${window.alphaAPIKey}`
   })
 }
