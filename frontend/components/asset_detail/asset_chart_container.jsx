5d647827dac4a738ca1fd0d9aa5ac7f1be778173
Laney <laneyluong@gmail.com>
2022-03-31 18:47:31 -0700

    remove unused code

--- frontend/components/asset_detail/asset_chart_container.jsx
+++ frontend/components/asset_detail/asset_chart_container.jsx
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
-import { fetchAssetFull, fetchCryptoFull, fetchAssetDaily } from '../../actions/asset_actions';
-import { createTransaction, fetchTransactions } from '../../actions/transaction_action';
+import { createTransaction } from '../../actions/transaction_action';
 import AssetChart from './asset_chart';
 
 const mapStateToProps = (state, ownProps) => {
@@ -13,12 +12,7 @@ const mapStateToProps = (state, ownProps) => {
   }
 
 const mapDispatchToProps = dispatch => ({
-  // fetchAssetFull: (symbol) => dispatch(fetchAssetFull(symbol)),
-  // fetchCryptoFull: (symbol) => dispatch(fetchCryptoFull(symbol)),
-  // fetchAssetInterval: (symbol) => dispatch(fetchAssetInterval(symbol)),
-  // fetchCryptoInterval: (symbol) => dispatch(fetchCryptoInterval(symbol)),
   createTransaction: (transaction) => dispatch(createTransaction(transaction)),
-  // fetchTransactions: (userId, interval) => dispatch(fetchTransactions(userId, interval)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AssetChart);
